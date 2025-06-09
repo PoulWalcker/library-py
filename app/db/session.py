14e98967f5e81ee74d67ac17ca20b1269a87e160
PaulWalcker <pashasimanko123@gmail.com>
2025-06-09 21:43:01 +0400

    feature: add sessions and dependencies for db

---
+++ app/db/session.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import settings
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
